9948d52dd44c76d61dfc4275faa35cdb385c3e5f
Lumix26 <marco.valerioti01@gmail.com>
2023-05-13 12:51:46 +0200

    aggiunta psw, username e id a Customer

--- src/main/java/com/example/mydemo/models/Customer.java
+++ src/main/java/com/example/mydemo/models/Customer.java
@@ -1,15 +1,30 @@
 package com.example.mydemo.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Clienti")
 public class Customer {
     
+    @Id
+    @SequenceGenerator(name = "cst_seq_id", allocationSize = 1)
+    @GeneratedValue(generator = "cst_seq_id", strategy = GenerationType.SEQUENCE)
+    private Long id;
 
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String psw;
+    
     
     /**
      * Many Customer To One registry (variabile di istanza)
